4eb3a2c3f4198383e1091ecacbdd8d1652720985
Antrikas <andreas.antoniades17@gmail.com>
2023-11-07 11:56:56 +0200

    JSHC

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
-function showRating(rating) {
-let ratings = "";
-for (let i = 0; i < Math.floor(rating); ++i){
-    ratings = ratings + "*"
-    if (i !== Math.floor(rating) -1) {
-        ratings = ratings + " "
-    }
-}
-if (!Number.isInteger(rating)){
-    ratings = ratings + " .";
-}
-return ratings
+//function showRating(rating) {
+//let ratings = "";
+//for (let i = 0; i < Math.floor(rating); ++i){
+  //  ratings = ratings + "*"
+    //if (i !== Math.floor(rating) -1) {
+      //  ratings = ratings + " "
+    //}
+//}
+//if (!Number.isInteger(rating)){
+  //  ratings = ratings + " .";
+//}
+//return ratings
+//}
+
+//console.log(showRating(4.5))
+
+//function sortLowToHigh(numbers) {
+  //  return numbers.sort((a, b) => b- a);
+//}
+    
+    
+    //console.log(sortLowToHigh([1, 5, 0, 10, 8]));
+    
+    //function sortHighToLow(numbers) {
+      //  return numbers.sort((a, b) => {
+        //    return b.price - a.price
+       // });
+    //}
+        
+        
+      //  console.log(sortHighToLow([
+        //    {id: 1, price: 50},
+          //  {id: 1, price: 0},
+            //{id: 1, price: 500}
+        //])
+       // );
+
+async function postsByUser (userId) {
+    const promise = await fetch("https://jsonplaceholder.typicode.com/posts")
+    
+    const result = await promise.json();
+
+    const posts = result.filter(element => element.userId === userId)
+
+    console.log(posts)
 }
 
-console.log(showRating(4.5))
+postsByUser (4);
